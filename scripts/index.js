23a1e482d7fc64cf302fba786dc5af18c14ba1ec
Andrey Lazarev <dronoti@yandex.ru>
2022-03-05 22:06:59 +0300

    fix: review notes

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-let editProfileBotton = document.querySelector('.profile__edit-button');
-let closeFormBotton = document.querySelector('.popup__close');
+let editProfileButton = document.querySelector('.profile__edit-button');
+let closeFormButton = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_name');
-let jobInput = formElement.querySelector('.popup__input_job');
+let nameInput = formElement.querySelector('.popup__input_profile_name');
+let jobInput = formElement.querySelector('.popup__input_profile_job');
 let userName = document.querySelector('.profile__name');
 let userJob = document.querySelector('.profile__description');
 
-editProfileBotton.addEventListener('click', () => {
+editProfileButton.addEventListener('click', () => {
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
   popup.classList.add('popup_opened')
 });
-closeFormBotton.addEventListener('click', () => popup.classList.remove('popup_opened'));
+closeFormButton.addEventListener('click', () => popup.classList.remove('popup_opened'));
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
